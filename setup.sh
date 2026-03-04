4f2dae2712aba957cb1211b91e3ef07b0971b9da
Wachter, Julian <julian.wachter@haufe-lexware.com>
2026-03-04 09:22:10 +0100

    app

--- setup.sh
+++ setup.sh
@@ -12,8 +12,4 @@ helm install argocd argo/argo-cd --namespace argocd --create-namespace --wait
 export CLUSTER_TOPOLOGY=true
 clusterctl init --infrastructure docker --kubeconfig ~/.kube/argocd-issue-11972-config
 
-clusterctl generate cluster capi-quickstart --flavor development \
-  --kubernetes-version v1.35.0 \
-  --control-plane-machine-count=1 \
-  --worker-machine-count=1 \
-  > capi-quickstart.yaml
+kubectl apply -f argocd-app.yaml
